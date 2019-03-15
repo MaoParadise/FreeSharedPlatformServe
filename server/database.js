557f022949ec1a5d1fb01b92051a07ab2948fbc2
MaoParadise <carlos92.pastenbravo@gmail.com>
2019-03-15 13:29:49 -0300

    primer subida al servidor

--- server/database.js
+++ server/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const URI = 'mongodb://localhost/bakodb'
+const URI = 'mongodb+srv://MaoParadise:AA57145894broodwar--@cluster0-fib7v.mongodb.net/test?retryWrites=true'
 
 mongoose.connect(URI, { useNewUrlParser: true })
     .then(db => console.log('DB is connected'))
